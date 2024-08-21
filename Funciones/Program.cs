f65546828754e262dff30b531e8934fd11b8b575
Jaify30 <josuevillavicenciodev@gmail.com>
2024-08-21 17:02:00 -0300

    Agregar archivos de proyecto.

---
+++ Funciones/Program.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Funciones
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static SqlConnection conexionBBDD() //Se crea la conexion a la base de datos
+        {
+            SqlConnection conexion = new SqlConnection("Server=DESKTOP-3MSS1LQ\\JAIFY3; Database=Hospital;" +
+                " Trusted_Connection=true; Integrated Security=SSPI;Persist Security Info=False;");
+            conexion.Open();
+
+            return conexion;
+        }
+        public static int RegistrarDoctores(Entidades.Program.Doctores Doctores, Entidades.Program.Empleados Empleado)
+        {
+            int retorno = 0;
+
+            using (SqlConnection conexion = conexionBBDD())
+            {
+                using (SqlTransaction transaccion = conexion.BeginTransaction())
+                {
+                    try
+                    {
+                        string queryEmpleado = @"
+                INSERT INTO Empleados (Nombre, Apellido, Password, Email, Nacionalidad, FechaNacimiento, FechaIngreso, Permiso, Token, Telefono, Domicilio, Localidad, Documento)
+                VALUES (@Nombre, @Apellido, @Password, @Email, @Nacionalidad, @FechaNacimiento, @FechaIngreso, @Permiso, @Token, @Telefono, @Domicilio, @Localidad, @Documento);
+                SELECT SCOPE_IDENTITY();";
+
+                        SqlCommand cmdEmpleado = new SqlCommand(queryEmpleado, conexion, transaccion);
+                        cmdEmpleado.Parameters.AddWithValue("@Nombre", Empleado.Nombre);
+                        cmdEmpleado.Parameters.AddWithValue("@Apellido", Empleado.Apellido);
+                        cmdEmpleado.Parameters.AddWithValue("@Password", Empleado.Password);
+                        cmdEmpleado.Parameters.AddWithValue("@Email", Empleado.Email);
+                        cmdEmpleado.Parameters.AddWithValue("@Nacionalidad", Empleado.Nacionalidad);
+                        cmdEmpleado.Parameters.AddWithValue("@FechaNacimiento", Empleado.FechaNacimiento);
+                        cmdEmpleado.Parameters.AddWithValue("@FechaIngreso", Empleado.FechaIngreso);
+                        cmdEmpleado.Parameters.AddWithValue("@Permiso", Empleado.Permiso);
+                        cmdEmpleado.Parameters.AddWithValue("@Token", Empleado.Token);
+                        cmdEmpleado.Parameters.AddWithValue("@Telefono", Empleado.Telefono);
+                        cmdEmpleado.Parameters.AddWithValue("@Domicilio", Empleado.Domicilio);
+                        cmdEmpleado.Parameters.AddWithValue("@Localidad", Empleado.Localidad);
+                        cmdEmpleado.Parameters.AddWithValue("@Documento", Empleado.Documento);
+
+                        int IdEmpleado = Convert.ToInt32(cmdEmpleado.ExecuteScalar());
+
+                        if (Doctores != null)
+                        {
+                            string queryDoctor = @"
+                    INSERT INTO Doctores (IdDoctor, Cargo, Matricula)
+                    VALUES (@IdDoctor, @Cargo, @Matricula);";
+
+                            SqlCommand cmdDoctor = new SqlCommand(queryDoctor, conexion, transaccion);
+                            cmdDoctor.Parameters.AddWithValue("@IdDoctor", IdEmpleado);
+                            cmdDoctor.Parameters.AddWithValue("@Cargo", Doctores.Cargo);
+                            cmdDoctor.Parameters.AddWithValue("@Matricula", Doctores.Matricula);
+
+                            cmdDoctor.ExecuteNonQuery();
+                        }
+
+                        //confirma la transaccion
+                        transaccion.Commit();
+                        retorno = IdEmpleado; // Devuelve el ID del empleado insertado
+                    }
+                    catch
+                    {
+                        transaccion.Rollback();
+                        throw;
+                    }
+                }
+            }
+            return retorno;
+        }
+        public static int RegistrarAuxiliares(Entidades.Program.EmpleadosAux empleadosAux, Entidades.Program.Empleados Empleado)
+        {
+            int retorno = 0;
+
+            using (SqlConnection conexion = conexionBBDD())
+            {
+                using (SqlTransaction transaccion = conexion.BeginTransaction())
+                {
+                    try
+                    {
+                        string queryEmpleado = @"
+                        INSERT INTO Empleados (Nombre, Apellido, Password, Email, Nacionalidad, FechaNacimiento, FechaIngreso, Permiso, Token, Telefono, Domicilio, Localidad, Documento)
+                        VALUES (@Nombre, @Apellido, @Password, @Email, @Nacionalidad, @FechaNacimiento, @FechaIngreso, @Permiso, @Token, @Telefono, @Domicilio, @Localidad, @Documento);
+                        SELECT SCOPE_IDENTITY();";
+
+                        SqlCommand cmdEmpleado = new SqlCommand(queryEmpleado, conexion, transaccion);
+                        cmdEmpleado.Parameters.AddWithValue("@Nombre", Empleado.Nombre);
+                        cmdEmpleado.Parameters.AddWithValue("@Apellido", Empleado.Apellido);
+                        cmdEmpleado.Parameters.AddWithValue("@Password", Empleado.Password);
+                        cmdEmpleado.Parameters.AddWithValue("@Email", Empleado.Email);
+                        cmdEmpleado.Parameters.AddWithValue("@Nacionalidad", Empleado.Nacionalidad);
+                        cmdEmpleado.Parameters.AddWithValue("@FechaNacimiento", Empleado.FechaNacimiento);
+                        cmdEmpleado.Parameters.AddWithValue("@FechaIngreso", Empleado.FechaIngreso);
+                        cmdEmpleado.Parameters.AddWithValue("@Permiso", Empleado.Permiso);
+                        cmdEmpleado.Parameters.AddWithValue("@Token", Empleado.Token);
+                        cmdEmpleado.Parameters.AddWithValue("@Telefono", Empleado.Telefono);
+                        cmdEmpleado.Parameters.AddWithValue("@Domicilio", Empleado.Domicilio);
+                        cmdEmpleado.Parameters.AddWithValue("@Localidad", Empleado.Localidad);
+                        cmdEmpleado.Parameters.AddWithValue("@Documento", Empleado.Documento);
+
+                        int IdEmpleado = Convert.ToInt32(cmdEmpleado.ExecuteScalar());
+
+                        if (empleadosAux != null)
+                        {
+                            string queryDoctor = @"
+                            INSERT INTO EmpleadosAux (IdEmpleado, Cargo)
+                            VALUES (@IdEmpleado, @Cargo);";
+
+                            SqlCommand cmdDoctor = new SqlCommand(queryDoctor, conexion, transaccion);
+                            cmdDoctor.Parameters.AddWithValue("@IdEmpleado", IdEmpleado);
+                            cmdDoctor.Parameters.AddWithValue("@Cargo", empleadosAux.Cargo);
+
+                            cmdDoctor.ExecuteNonQuery();
+                        }
+
+                        //confirma la transaccion
+                        transaccion.Commit();
+                        retorno = IdEmpleado; // Devuelve el ID del empleado insertado
+                    }
+                    catch
+                    {
+                        transaccion.Rollback();
+                        throw;
+                    }
+                }
+            }
+            return retorno;
+        }
+        public static bool VerificarFecha(DateTime? fecha, string nombreCampo)
+        {
+            // Verifica si la fecha ha sido seleccionada
+            if (!fecha.HasValue)
+            {
+                MessageBox.Show($"Por favor, seleccione una fecha para {nombreCampo}.", "Fecha Requerida", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            // Verifica que la fecha no sea mayor a la fecha actual
+            if (fecha.Value > DateTime.Now)
+            {
+                MessageBox.Show($"La fecha de {nombreCampo} no puede ser mayor a la fecha actual.", "Fecha Inválida", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return false;
+            }
+
+            // Si todo es válido
+            return true;
+        }
+        public static bool ValidarLongitudTexto(TextBox textBox, int minLongitud, int maxLongitud, string nombreCampo, string textoValidado)
+        {
+            textoValidado = string.Empty;
+
+            if (textBox.Text.Length < minLongitud)
+            {
+                MessageBox.Show($"Ingrese un {nombreCampo}, por favor.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            if (textBox.Text.Length > maxLongitud)
+            {
+                MessageBox.Show($"Se ha excedido del numero maximo de caracteres[{maxLongitud}] ({nombreCampo}).", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            textoValidado = textBox.Text;
+            return true;
+        }
+        public static bool ValidarEmail(string email)
+        {
+            // Expresión regular para validar direcciones de correo electrónico
+            string patronEmail = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            Regex regex = new Regex(patronEmail);
+
+            // Verificar si el email coincide con el patrón
+            if (regex.IsMatch(email))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show("La dirección de correo electrónico no es válida. Tiene que tener formato Email pepe123@ejemplo.com", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
+        public static bool ValidarContrasena(string contrasena, string confirmarContrasena)
+        {
+            // Verificar si la contraseña tiene al menos 8 caracteres
+            if (contrasena.Length < 8 || confirmarContrasena.Length < 8)
+            {
+                MessageBox.Show("La contraseña debe tener al menos 8 caracteres.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            // Verificar si la confirmación de la contraseña coincide con la original
+            if (contrasena != confirmarContrasena)
+            {
+                MessageBox.Show("Las contraseñas no coinciden.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+
+            return true;
+        }
+        public static string GenerarContrasenaAleatoria(int longitud)
+        {
+            const string caracteresPermitidos = "1234567890";
+            var random = new Random();
+            return new string(Enumerable.Repeat(caracteresPermitidos, longitud)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        public static bool ValidarSoloNumeros(string numero, out int numeroValidado, string NombreCampo)
+        {
+            // Intentar convertir el texto a un número entero
+            if (int.TryParse(numero, out numeroValidado))
+            {
+                return true;
+            }
+            else
+            {
+                MessageBox.Show($"Por favor, ingrese solo números enteros. ({NombreCampo})", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+        }
+        public static bool EsCorreoValido(string Email)
+        {
+            if (string.IsNullOrWhiteSpace(Email))
+            {
+                return false;
+            }
+
+            try
+            {
+                string patron = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+                Regex regex = new Regex(patron,RegexOptions.IgnoreCase);
+                return regex.IsMatch(Email);
+            }
+            catch (RegexMatchTimeoutException)
+            {
+                return false;
+            }
+        }
+        public static int VerificacionLogin(Entidades.Program.Empleados usuario)
+        {
+            int retorno = 0;
+
+            using (SqlConnection conexion = conexionBBDD())
+            {
+                string query = "SELECT COUNT(1) FROM Empleados WHERE Email = @Email AND Pass COLLATE SQL_Latin1_General_CP1_CS_AS = @Password";//Hacemos una query donde se seleccione solo email y contrasena
+
+                SqlCommand command = new SqlCommand(query, conexion);
+
+                command.Parameters.AddWithValue("@Email", usuario.Email);
+                command.Parameters.AddWithValue("@Pass", usuario.Password);
+
+                try
+                {
+                    retorno = (int)command.ExecuteScalar();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Ha ocurrido un error.");
+                }
+
+                return retorno;
+            }
+        }
+        public static int IngresoToken(Entidades.Program.Empleados Usuario)
+        {
+            int retorno = 0;
+
+            using (SqlConnection conexion = conexionBBDD())
+            {
+                string query = "SELECT COUNT(*) FROM Empleados WHERE Token = @Token";
+
+                SqlCommand cmd = new SqlCommand(query, conexion);
+
+                cmd.Parameters.AddWithValue("@Token", Usuario.Token);
+
+                try
+                {
+                    retorno = (int)cmd.ExecuteScalar();
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show("Ha ocurrido un error: " + e.Message);
+                }
+
+                return retorno;
+            }
+        }
+    }
+}
---
+++ SeleccionRegistro/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SeleccionRegistro
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void SeleccionMedicos_Click(object sender, RoutedEventArgs e)
+        {
+            RegistroMed.MainWindow registroMed = new RegistroMed.MainWindow();
+            this.Close();
+            registroMed.ShowDialog();
+        }
+
+        private void SeleccionAuxiliares_Click(object sender, RoutedEventArgs e)
+        {
+            RegistroAux.MainWindow registroAux = new RegistroAux.MainWindow();
+            this.Close();
+            registroAux.ShowDialog();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+    }
+}
